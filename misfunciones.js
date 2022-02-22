ed8f56a2d148d53f5652b45e11775a97cb06f60d
Martin Molina <jmmolinaes@gmail.com>
2022-02-22 13:00:44 -0600

    Primera versión

---
+++ misfunciones.js
@@ -0,0 +1,9 @@
+var hola = "Hola Mundo"
+
+function procesarFormulario(){
+	var nombre = document.getElementById("nombre").value
+	document.getElementById("salida").innerHTML = 
+	"Gracias, "+nombre+" , pronto me prondre en contacto!";
+}
+
+//
